77bb2b368627b5eb560caba89412f55b1611513a
Mohd Taqiuddin <taqiuddin12345@gmail.com>
2022-02-02 23:45:58 +0530

    routers

---
+++ src/pages/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+  return(
+    <div className='home'>
+      
+      <div className='home__header'></div>
+
+
+      <div className='home__body'></div>
+
+
+      <div className='home__footer'></div>
+
+
+    </div>
+  ) 
+}
+
+export default Home;
